7fb1e3380212386296aaffe1ef20a82847d52be8
Steve Jo <trilliwon@gmail.com>
2016-12-14 11:57:53 +0900

    first commit

---
+++ Example/NaturalKorean_ExampleTests/NaturalKorean+StringExtensionTests.swift
@@ -0,0 +1,9 @@
+//
+//  NaturalKorean+StringExtensionTests.swift
+//  NaturalKorean
+//
+//  Created by Steve Jo on 14/12/2016.
+//  Copyright © 2016 CocoaPods. All rights reserved.
+//
+
+import Foundation
